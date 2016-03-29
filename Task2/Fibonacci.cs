adf7fc5a4f13ced37bf6f413a630d5485235538a
Natalie <natalie.pakki@gmail.com>
2016-03-29 17:55:00 +0300

    add Task2

---
+++ Task2/Fibonacci.cs
@@ -0,0 +1,17 @@
+﻿
+using System.Collections.Generic;
+
+namespace Task2 {
+    public static class Fibonacci {
+        public static IEnumerable<int> GetNumbers(int size) {
+            int prev = 1;
+            int curr = 1;
+            for (int i = 0; i < size; i++) {
+                yield return prev;
+                int next = prev + curr;
+                prev = curr;
+                curr = next;
+            }
+        } 
+    }
+}
