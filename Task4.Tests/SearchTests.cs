06c8666573201a2aa1c8cd393e198ad6f147459e
Natalie <natalie.pakki@gmail.com>
2016-03-30 14:43:30 +0300

    add Task4 and Task4.Tests

---
+++ Task4.Tests/SearchTests.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Task4.Tests {
+    [TestFixture]
+    public class SearchTests {
+        private int[] array = {-4, 1, 4, 7, 12, 56};
+        private string[] names = {"Ann", "Danill", "Melanie","Zoyee"};
+
+
+        public static IEnumerable<TestCaseData> TestIntDatas {
+            get {
+                yield return new TestCaseData(-5).Returns(false);
+                yield return new TestCaseData(100).Returns(false);
+                yield return new TestCaseData(0).Returns(false);
+                yield return new TestCaseData(4).Returns(true);
+                yield return new TestCaseData(56).Returns(true);
+            }
+        }
+
+        public IEnumerable<TestCaseData> TestStringDatas {
+            get {
+
+                yield return new TestCaseData("Natalie").Returns(false);
+                yield return new TestCaseData("Melanie").Returns(true);
+                yield return new TestCaseData("Zoyee").Returns(true);
+                yield return new TestCaseData("Zick").Returns(false);
+            }
+        }
+
+        [Test, TestCaseSource(nameof(TestIntDatas))]
+        public bool BinarySearchTest_Int(int x) {
+            return Search.BinarySearch(array, x);
+        }
+        [Test, TestCaseSource(nameof(TestStringDatas))]
+        public bool BinarySearchTest_String(string x) {
+            return Search.BinarySearch(names, x);
+        }
+    }
+}
---
+++ Task4/Search.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Task4 {
+    public static class Search {
+        public static bool BinarySearch<T>(T[] array, IComparable<T> x) {
+            if (x.CompareTo(array[0]) < 0 || x.CompareTo(array[array.Length - 1]) > 0)
+                    return false;
+                return Find(array, x, 0, array.Length - 1); 
+        }
+
+        private static bool Find<T>(T[] a, IComparable<T> x, int l, int r) {
+            if (l > r)
+                return false;
+            int m = (l + r) / 2;
+            if (x.CompareTo(a[m]) == 0)
+                return true;
+            if (x.CompareTo(a[m]) < 0)
+                return Find(a, x, l, m - 1);
+            if (x.CompareTo(a[m]) > 0)
+                return Find(a, x, m + 1, r);
+            return false;
+        }
+    }
+}
