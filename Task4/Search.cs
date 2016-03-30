e6515cf57ab9e36eb4a16cccc942d54f8da9dec0
Natalie <natalie.pakki@gmail.com>
2016-03-30 22:57:04 +0300

    refactoring

--- Task4/Search.cs
+++ Task4/Search.cs
@@ -4,11 +4,9 @@ using System.Collections.Generic;
 namespace Task4 {
     public static class Search {
         public static bool BinarySearch<T>(T[] array, T x, IComparer<T> comparer = null) {
-            if(array == null) throw new ArgumentNullException($"{nameof(array)} is null");
-            if (comparer != null) return Find(array, x, 0, array.Length - 1, comparer);
-            var comparable = x as IComparable<T>;
-            if (comparable == null) throw new ArgumentException("Values mustn't comparer.");
-            return Find(array, comparable, 0, array.Length - 1);
+            if (array == null) throw new ArgumentNullException($"{nameof(array)} is null");
+            if (comparer == null) comparer = Comparer<T>.Default;
+            return Find(array, x, 0, array.Length - 1,comparer);
         }
 
         private static bool Find<T>(T[] a, T x, int l, int r, IComparer<T> comparer) {
@@ -23,22 +21,6 @@ namespace Task4 {
                 return Find(a, x, l, m - 1,comparer);
             if (comparer.Compare(x,a[m]) > 0)
                 return Find(a, x, m + 1, r,comparer);
-            return false;
-        }
-
-
-        private static bool Find<T>(T[] a, IComparable<T> x, int l, int r) {
-            if (l > r)
-                return false;
-            if (x.CompareTo(a[l]) < 0 || x.CompareTo(a[r]) > 0)
-                return false;
-            int m = (l + r) / 2;
-            if (x.CompareTo(a[m]) == 0)
-                return true;
-            if (x.CompareTo(a[m]) < 0)
-                return Find(a, x, l, m - 1);
-            if (x.CompareTo(a[m]) > 0)
-                return Find(a, x, m + 1, r);
             return false;
         }
     }
--- Task5/Sort.cs
+++ Task5/Sort.cs
@@ -1,28 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace Task5 {
     public static class Sort{
         public static void BubbleSort<T>(T[] array, IComparer<T> comparer = null){
             if (array == null) throw new ArgumentNullException($"{nameof(array)} is null");
             if (comparer == null) {
-                    BubbleSort(array);
-            } else {
+                comparer = Comparer<T>.Default;
                 for (int i = 0; i < array.Length - 1; i++)
                     for (int j = 0; j < array.Length - i - 1; j++) {
-                        if (comparer.Compare(array[j], array[j + 1]) < 0) {
+                        if (comparer.Compare(array[j], array[j + 1]) > 0) {
                             Swap(ref array[j], ref array[j + 1]);
                         }
                     }
             }
-        }
 
+        }
         public static void SelectionSort<T>(T[] array, IComparer<T> comparer = null) {
             if (array == null) throw new ArgumentNullException($"{nameof(array)} is null");
             if (comparer == null) {
-                    SelectionSort(array);
-            } else {
+                comparer = Comparer<T>.Default;
                 for (int i = 0; i < array.Length - 1; i++) {
                     int min = i;
                     for (int j = i + 1; j < array.Length; j++)
@@ -35,32 +32,6 @@ namespace Task5 {
             }
         }
 
-        private static void BubbleSort<T>(T[] array) {
-            if (array.All(x => x is IComparable<T>)) {
-                for (int i = 0; i < array.Length - 1; i++)
-                    for (int j = 0; j < array.Length - i - 1; j++) {
-                        if (((IComparable<T>) array[j]).CompareTo(array[j + 1]) > 0) {
-                            Swap(ref array[j], ref array[j + 1]);
-                        }
-                    }
-            }
-            else throw new ArgumentException("Values mustn't comparer");
-        }
-
-        private static void SelectionSort<T>(T[] array) {
-            if (array.All(x => x is IComparable<T>)) {
-                for (int i = 0; i < array.Length - 1; i++) {
-                    int min = i;
-                    for (int j = i + 1; j < array.Length; j++)
-                        if (((IComparable<T>) array[j]).CompareTo(array[min]) < 0)
-                            min = j;
-                    if (min != i) {
-                        Swap(ref array[i], ref array[min]);
-                    }
-                }
-            }
-            else throw new ArgumentException("Values mustn't comparer");
-        }
 
         private static void Swap<T>(ref T x, ref T y){
             T temp = x;
